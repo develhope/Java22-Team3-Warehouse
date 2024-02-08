c5f9299f5c1a0140a48ac6bc5b5cc645876ab21b
Lutti Davide <luttidavide004@gmail.com>
2024-02-08 11:33:39 +0100

    Add random ID

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,7 +1,5 @@
-
 public class Notebook extends Dispositivo {
-
-    public Notebook(int id, String produttore, String modello, String descrizione, String pollici, String spazioArchiviazione, float prezzoAcquisto, float prezzoVendita, TipoDispositivo tipoDispositivo) {
-        super(id, produttore, modello, descrizione, pollici, spazioArchiviazione, prezzoAcquisto, prezzoVendita, tipoDispositivo);
+    public Notebook(String produttore, String modello, String descrizione, String pollici, String spazioArchiviazione, float prezzoAcquisto, float prezzoVendita, TipoDispositivo tipoDispositivo) {
+        super(produttore, modello, descrizione, pollici, spazioArchiviazione, prezzoAcquisto, prezzoVendita, tipoDispositivo);
     }
 }
