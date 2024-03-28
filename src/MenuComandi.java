f9ea3714683065d142a33f5f1fc98a6949ba10f2
Bisco <alidero1605@gmail.com>
2024-03-28 15:50:50 +0100

    aggiunta Enum al tipoDispositivo // Fix Tablet

--- src/MenuComandi.java
+++ src/MenuComandi.java
@@ -480,7 +480,7 @@ public class MenuComandi {
     public TipoDispositivo impostaDispositivo() throws Exception {
         Scanner scanner = new Scanner(System.in);
         TipoDispositivo tipoDispositivo = null;
-        System.out.println("Inserisci il tipo di dispositivo (1 per Smartphone, 2 per Tablete, 3 per Notebook):");
+        System.out.println("Inserisci il tipo di dispositivo (1 per Smartphone, 2 per Tablet, 3 per Notebook):");
         while (tipoDispositivo == null) {
             try {
                 int input = scanner.nextInt();
@@ -505,5 +505,4 @@ public class MenuComandi {
 
         return tipoDispositivo;
     }
-
 }
